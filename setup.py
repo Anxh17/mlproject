2da75413bc62370dacbb118dfab434c8779a6406
Anxh17 <ansh.dixit2006@gmail.com>
2026-04-14 00:25:51 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT="-e ."
+def get_requirements(filepath:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(filepath) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements    
+
+setup(
+    name='mlproject',
+    version= '0.0.1',
+    author='Ansh',
+    author_email='ansh.dixit2006@gmail.com',
+    pakages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
